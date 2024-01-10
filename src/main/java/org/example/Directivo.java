fb14aa05345404c9d4ae849e959004674756dee2
alorenzot <alejandrolorenzotoledo@gmail.com>
2024-01-10 19:17:55 +0100

    Ejercicios

---
+++ src/main/java/org/example/Directivo.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Directivo extends Profesor{
+    private String cargo;
+    private TipoEstudio[] gradosImpartidos;
+
+    public Directivo(String nombre, String apellidos, String DNI, int edad, String cargo, TipoEstudio... gradosImpartidos) {
+        super(nombre, apellidos, DNI, edad);
+        this.cargo = cargo;
+        this.gradosImpartidos = gradosImpartidos;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void imprimirDNI() {
+        System.out.println("Directivo: " + getDNI());
+    }
+
+    @Override
+    public String toString() {
+        return "Directivo{ " +
+                "nombre='" + getNombre() + '\'' +
+                ", apellidos='" + getApellidos() + '\'' +
+                ", DNI='" + getDNI() + '\'' +
+                ", edad=" + getEdad() +
+                ", cargo= " + cargo + "\n" +
+                ", Grados impartidos= " + Arrays.toString(gradosImpartidos) + "\n" +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Profesor.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Profesor extends Persona{
+    private TipoEstudio[] gradosImpartidos;
+
+    public Profesor(String nombre, String apellidos, String DNI, int edad, TipoEstudio... gradosImpartidos) {
+        super(nombre, apellidos, DNI, edad);
+        this.gradosImpartidos = gradosImpartidos;
+    }
+
+    public TipoEstudio[] getGradosImpartidos() {
+        return gradosImpartidos;
+    }
+
+    public void setGradosImpartidos(TipoEstudio[] gradosImpartidos) {
+        this.gradosImpartidos = gradosImpartidos;
+    }
+
+    @Override
+    public void imprimirDNI() {
+        System.out.println("Profesor: " + getDNI());
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{ " +
+                "nombre='" + getNombre() + '\'' +
+                ", apellidos='" + getApellidos() + '\'' +
+                ", DNI='" + getDNI() + '\'' +
+                ", edad=" + getEdad() + "\n" +
+                ", Grados impartidos= " + Arrays.toString(gradosImpartidos) + "\n" +
+                '}';
+    }
+}
