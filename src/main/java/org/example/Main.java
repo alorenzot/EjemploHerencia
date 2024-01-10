bc92c2d50090f216fd31710d4ba61a547061fb95
alorenzot <alejandrolorenzotoledo@gmail.com>
2024-01-10 18:38:27 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Manolo", "Garcia", "123", 33);
+        Persona p2 = new Persona("Manolo", "Garcia", "123", 33);
+        System.out.println(p1.equals(p2));
+
+    }
+}
