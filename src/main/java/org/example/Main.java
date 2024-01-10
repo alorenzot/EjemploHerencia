200aa2a6994429c197b9c1cecaef61d34255df4d
alorenzot <alejandrolorenzotoledo@gmail.com>
2024-01-10 18:46:01 +0100

    Ejercicios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Persona p1 = new Persona("Manolo", "Garcia", "123", 33);
         Persona p2 = new Persona("Manolo", "Garcia", "123", 33);
+
         System.out.println(p1.equals(p2));
 
     }
